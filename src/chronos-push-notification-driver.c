3963d36cd7af54dc8b196e29888e739ea32ea598
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2022-08-17 17:24:27 +0200

    chronos-push-notification-driver: Implement config parsing

--- src/chronos-push-notification-driver.c
+++ src/chronos-push-notification-driver.c
@@ -1,10 +1,19 @@
 /* Copyright (c) 2022 Open-Xchange Software GmbH, see the included COPYING file */
 
 #include "lib.h"
+#include "hash.h"
 #include "http-client.h"
+#include "http-url.h"
 #include "iostream-ssl.h"
 #include "push-notification-drivers.h"
 #include "push-notification-plugin.h"
+#include "settings-parser.h"
+#include "str.h"
+
+/* Default values. */
+#define DEFAULT_MSG_MAX_SIZE 512000
+#define DEFAULT_RETRY_COUNT 1
+#define DEFAULT_TIMEOUT_MSECS 2000
 
 struct chronos_push_notification_driver_global {
 	struct http_client *http_client;
@@ -15,8 +24,10 @@ static struct chronos_push_notification_driver_global *chronos_global = NULL;
 
 struct chronos_push_notification_driver_config {
 	struct event *event;
+	struct http_url *http_url;
 	unsigned int http_max_retries;
 	unsigned int http_timeout_msecs;
+	uoff_t msg_max_size;
 };
 
 static void
@@ -63,16 +74,80 @@ chronos_push_notification_driver_global_unref(void)
 
 static int
 chronos_push_notification_driver_init(
-	struct push_notification_driver_config *config ATTR_UNUSED,
-	struct mail_user *user, pool_t pool,
-	void **context, const char **error_r ATTR_UNUSED)
+	struct push_notification_driver_config *config,
+	struct mail_user *user, pool_t pool, void **context,
+	const char **error_r)
 {
 	struct chronos_push_notification_driver_config *dconfig;
+	const char *config_item, *error;
 
 	dconfig = p_new(pool, struct chronos_push_notification_driver_config, 1);
 	dconfig->event = event_create(user->event);
 	event_add_category(dconfig->event, push_notification_get_event_category());
 	event_set_append_log_prefix(dconfig->event, "push-notification-chronos: ");
+
+	config_item = hash_table_lookup(config->config, (const char *)"url");
+	if (config_item == NULL) {
+		e_error(dconfig->event, "URL config missing");
+		event_unref(&dconfig->event);
+		*error_r = "Driver requires the url parameter";
+		return -1;
+	}
+
+	if (http_url_parse(config_item, NULL, HTTP_URL_ALLOW_USERINFO_PART,
+			   pool, &dconfig->http_url, &error) < 0) {
+		event_unref(&dconfig->event);
+		*error_r = t_strdup_printf(
+			"Failed to parse Chronos Push Notification URL '%s': %s",
+			config_item, error);
+		return -1;
+	}
+
+	config_item = hash_table_lookup(config->config, (const char *)"max_retries");
+	if ((config_item == NULL) ||
+	    (str_to_uint(config_item, &dconfig->http_max_retries) < 0)) {
+		if (config_item == NULL) {
+			config_item = "(unset)";
+		}
+		e_warning(dconfig->event,
+			  "Unable to parse setting for \"max_retries\" value: %s. "
+			  "Using default value: 1.", config_item);
+		dconfig->http_max_retries = DEFAULT_RETRY_COUNT;
+	}
+
+	config_item = hash_table_lookup(config->config, (const char *)"timeout");
+	if ((config_item == NULL) ||
+	    (settings_get_time_msecs(config_item, &dconfig->http_timeout_msecs, &error) < 0)) {
+		if (config_item == NULL) {
+			config_item = "(unset)";
+			error = "";
+		}
+		e_warning(dconfig->event,
+			  "Unable to parse setting for \"timeout\" value: %s. "
+			  "Using default value: %dms. %s",
+			  config_item, DEFAULT_TIMEOUT_MSECS, error);
+		dconfig->http_timeout_msecs = DEFAULT_TIMEOUT_MSECS;
+	}
+
+	config_item = hash_table_lookup(config->config, (const char *)"msg_max_size");
+	if ((config_item == NULL) ||
+	    (settings_get_size(config_item, &dconfig->msg_max_size, &error) < 0)) {
+		if (config_item == NULL) {
+			config_item = "(unset)";
+			error = "";
+		}
+		e_warning(dconfig->event,
+			  "Unable to parse setting for \"msg_max_size\" value: %s. "
+			  "Using default value: %dB. %s",
+			  config_item, DEFAULT_MSG_MAX_SIZE, error);
+		dconfig->msg_max_size = DEFAULT_MSG_MAX_SIZE;
+	}
+
+	e_debug(dconfig->event,
+		"Using config: max retries = %u, timeout = %ums, "
+		"msg max size = %"PRIuUOFF_T"B",
+		dconfig->http_max_retries, dconfig->http_timeout_msecs,
+		dconfig->msg_max_size);
 
 	if (chronos_global == NULL) {
 		chronos_push_notification_driver_init_global(dconfig, user);
